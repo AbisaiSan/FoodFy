a20265b2d45becfe163814311c0bfc6152b4ae7d
AbisaiSan <abisaiweb7@gmail.com>
2020-07-11 10:58:49 -0300

    Mudanças no js

--- js/receitas.js
+++ js/receitas.js
@@ -4,20 +4,29 @@ const cards = document.querySelectorAll('.card');
 
 for(let card of cards) {
 
-  const imgId = card.getAttribute('id')
+  //Setando o atributo a variavel
+  const imgId = card.getAttribute('id');
+  const title = card.querySelector('.nome-prato').innerText // atribui o texto que contém 
+  const author = card.querySelector('.chef').innerText
   
-
   card.addEventListener("click", function(){
+
     modalOverlay.classList.add('active') 
-    modalOverlay = document.querySelector(".img-receita").src = `/assets/${imgId}.png`
+
+
+
+    modalOverlay.querySelector("img").src = `/assets/${imgId}.png`
+
+    modalOverlay.querySelector('.descricao-receita').innerHTML = `${title}`
+    modalOverlay.querySelector('.chef-modal').innerHTML = `${author}`
     console.log('teste')
   })
 }
 
-
+//Fechando o modal
 const closeModal = document.querySelector('.close-modal')
 
 closeModal.addEventListener("click", function (){
     modalOverlay.classList.remove('active') // remove classe active p/ fechar modal
-    
+    modalOverlay.querySelector("img").src = ""
 })
